4c30e3c575b55e945f8aa95beabd01ed61dd1f56
X shadow <gialollog@gmail.com>
2021-07-29 17:24:14 +0200

    score controller e calcolo media rating

---
+++ Backend/src/main/java/com/sharingnotes/Controller/ScoreController.java
@@ -0,0 +1,39 @@
+package com.sharingnotes.Controller;
+
+import com.sharingnotes.MongoDb.MongoDb;
+import org.bson.BsonDocument;
+import org.bson.Document;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScoreController {
+
+    @PostMapping(value = "/updateScore")
+    public ResponseEntity<String> updateScore(@RequestParam("score") int score, @RequestParam("document")String document,@RequestParam("collection") String collection){
+        try {
+            MongoDb mongoDb=new MongoDb();
+            Document document1= Document.parse(document);
+            mongoDb.updateScore(document1,collection,score);
+            System.out.println("MEDIA VOTI: "+avgScore(document1));
+            return ResponseEntity.ok("Score aggiornato!");
+        }catch (Exception e){
+            return new ResponseEntity<>("Score non aggiornato", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public float avgScore(Document document){
+        int rating=document.getInteger("rating");
+        int nvoti=document.getInteger("nvoti");
+        float media=0.0f;
+        try {
+            media = Math.round(rating/ nvoti);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return media;
+    }
+}
--- Backend/src/main/java/com/sharingnotes/MongoDb/MongoDb.java
+++ Backend/src/main/java/com/sharingnotes/MongoDb/MongoDb.java
@@ -6,6 +6,8 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import com.sharingnotes.Model.Notes;
 import com.sharingnotes.Model.User;
 import org.bson.Document;
@@ -40,6 +42,10 @@ public class MongoDb {
         database.getCollection(collection).insertOne(insertUser);
     }
 
+    public void deleteDocument(Document document,String collection){
+        database.getCollection(collection).deleteOne(document);
+    }
+
     public void insertNotes(Notes notes, String collection){
         Document insertNotes = new Document();
         insertNotes.append("_id",notes.getId())
@@ -51,6 +57,13 @@ public class MongoDb {
                 .append("id_User", notes.getId_User());
 
         database.getCollection(collection).insertOne(insertNotes);
+    }
+
+    public void updateScore(Document document,String collection,int score){
+        int newNoti=(document.getInteger("nvoti")+1);
+        int newRating=(document.getInteger("rating")+score);
+        database.getCollection(collection).updateOne(Filters.eq("_id",document.get("_id")), Updates.set("nvoti",newNoti));
+        database.getCollection(collection).updateOne(Filters.eq("_id",document.get("_id")), Updates.set("rating",newRating));
     }
 
     public Document getUser(String email, String password, String collections){

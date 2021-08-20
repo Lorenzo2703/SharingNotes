2781a25df1101e5b334e312e733f4f28a684a902
X shadow <gialollog@gmail.com>
2021-08-20 15:35:06 +0200

    commit

--- Backend/src/main/java/com/sharingnotes/Controller/ChatController.java
+++ Backend/src/main/java/com/sharingnotes/Controller/ChatController.java
@@ -4,15 +4,13 @@ import com.sharingnotes.Model.Chat;
 import com.sharingnotes.MongoDb.MongoDb;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
+@CrossOrigin()
 @RestController
 public class ChatController {
 

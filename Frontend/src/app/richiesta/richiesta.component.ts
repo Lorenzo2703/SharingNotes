301c40206f7eaad96a2d2b91bab6cf4b2982c3fe
MarcoSciacovelli <marco.sciacovelli99@gmail.com>
2021-09-06 12:11:27 +0200

    Sistemato richiesta ma è da finire

--- Frontend/src/app/richiesta/richiesta.component.ts
+++ Frontend/src/app/richiesta/richiesta.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+import { AjaxService } from '../ajax.service';
 
 @Component({
   selector: 'app-richiesta',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RichiestaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private data: DataService, private ajaxService: AjaxService) { }
+
+  richiesta = {
+    testo: "",
+    title: "",
+    id_Richiedente: "",
+    _id: "",
+    completed: false,
+  };
+  autore;
 
   ngOnInit(): void {
+    this.getParam();
+  }
+
+  ngDoCheck(): void {
+    if (this.richiesta._id === "") {
+      this.getParam();
+    }
+  }
+
+  getParam() {
+    this.activatedRoute.params.subscribe(params => {
+      //prendo l'id della nota dall'url
+      let id = params['_id'];
+      //scorro la lista dei documenti e salvo quello che ho selezionato
+      this.ajaxService.getRequest().subscribe(res =>{
+        for(let x in res){
+          if(res[x]._id == id){
+            this.richiesta = res[x];
+            //scrivo il nome dell'autore della richiesta
+            this.ajaxService.getUserByID(res[x]["id_Richiedente"]).subscribe(res =>{
+              this.autore = res["name"];
+            })
+          }
+        }
+      })
+    });
   }
 
 }

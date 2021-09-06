fbb3b6d549713c807364d081ff8ceb3f1bdbe78c
MarcoSciacovelli <marco.sciacovelli99@gmail.com>
2021-09-06 11:04:49 +0200

    insertrichiesta

--- Frontend/src/app/new-richiesta/new-richiesta.component.ts
+++ Frontend/src/app/new-richiesta/new-richiesta.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { DataService } from '../data.service';
+import { Router } from '@angular/router';
+import { AjaxService } from '../ajax.service';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-new-richiesta',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewRichiestaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ajax: AjaxService,private dataservice: DataService, public dialogRef: MatDialogRef<HomeComponent>) { }
+  form;
+
+  initForm() {
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      testo: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submit() {
+    //inizializzo il form
+    const formData = new FormData();
+    //aggiungo i campi al form
+    formData.append("idRichiedente", sessionStorage.getItem("UserID"));
+    formData.append('title', this.form.get("title").value);
+    formData.append("testo", this.form.get("testo").value);  
+    //invio il form 
+    this.ajax.insertRequest(formData).subscribe((res) => {
+    });
+    this.dialogRef.close();
+    window.location.reload();
+  }
 
   ngOnInit(): void {
+    this.initForm();
+    
   }
 
 }
--- Frontend/src/app/request/request.component.ts
+++ Frontend/src/app/request/request.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+import { AjaxService } from '../ajax.service';
+import { MatDialog } from '@angular/material/dialog';
+import { NewRichiestaComponent } from '../new-richiesta/new-richiesta.component'
 
 @Component({
   selector: 'app-request',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private data: DataService, public dialog: MatDialog, private ajaxService: AjaxService) { }
+
+  listRequest = [];
 
   ngOnInit(): void {
+    this.getRequest();
+  }
+
+  getRequest(){
+    this.ajaxService.getRequest().subscribe(res =>{
+      for(let x in res){
+        this.listRequest.push(res[x]);
+      }
+
+    })
+  }
+
+  openDialog() {
+    //apre il new-chat component
+    const dialogRef = this.dialog.open(NewRichiestaComponent);
   }
 
 }
